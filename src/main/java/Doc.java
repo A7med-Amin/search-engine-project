2f776334e1a04d0271f5379d4417d4c1e6ecf030
A7med-Amin <aahmedameen2000@gmail.com>
2023-05-15 04:08:10 +0300

    Add indexer variables and helper functions

---
+++ src/main/java/Doc.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import java.util.ArrayList;
+public class Doc {
+    String docId;
+    int docLength;
+    int tf;
+    List<Integer> positions = new ArrayList<>();
+    String firstOccurance;
+}
